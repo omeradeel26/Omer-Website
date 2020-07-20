5fdf65b338e249b39f7728374e2100916fa449ed
omeradeel26 <57784751+omeradeel26@users.noreply.github.com>
2020-07-19 21:09:48 -0400

    Initial commit

---
+++ homepage_script.js
@@ -0,0 +1,26 @@
+var x = document.querySelectorAll('.nav__links');
+var y = window.scrollY;
+
+var checkHeader = function(){
+    let scrollPosition = Math.round(window.scrollY);
+    
+    if (scrollPosition > 20){
+        document.querySelector('header').classList.add('sticky');
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.add('stick');
+        }
+        document.getElementById("nav__image").src = "imgs/logo.png";
+    } else {
+        document.querySelector('header').classList.remove('sticky');
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove('stick');
+        }
+        document.getElementById('nav__image').src = "imgs/logov2.png";
+    }
+}
+
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
+window.addEventListener('scroll',checkHeader);
