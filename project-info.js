f0f5ea883514b661063220b3b88ab257270ac1b2
Omer Adeel <oadeel@uwaterloo.ca>
2022-01-11 14:45:38 -0500

    added updated project gallery

---
+++ project-info.js
@@ -0,0 +1,241 @@
+data = {
+    "Checkers": {
+        name: "Checkers",
+        image: "url(./media/checkers.jpg)",
+        modal:{
+            name: "Checkers",
+            date: "01/2020",
+            summary: "Using Python Processing, I created the classic board game checkers. It features a full user menu, replayability, background music and high quality vector images. The project used a variety of different data types such as lists, dictionaries and tuples. This also has a heavily reliance upon the python processing graphics.",
+            lesson: "I learned to create a full functioning game in python processing. It taught me to merge graphics, use basic data-types, and different game ideas such as an infinite loop. However, there are many things I would like to improve on from this project such as creating re-usable code, using recursion for game mechanics and implementing object-oriented programming.",
+            github: "https://github.com/omeradeel26/Checkers",
+            video: "https://www.youtube.com/embed/IVNynBd6Cjk"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: 'processing',
+            year: "2020",
+            cont: "edu"
+        }
+    },
+    "Asteroids": {
+        name: "Asteroids",
+        image: "url(./media/asteroids.png)",
+        modal:{
+            name: "Asteroids",
+            date: "11/2020",
+            summary: "Using Python Processing, I created the classic Attari game asteroids. The goal of this game is to destroy asteroids and alien ships and keep your spaceship alive as long as possible. This game used different phyiscs mechanics such as inertia and was implemented using OOP and processing graphics.",
+            lesson: "From this project, I learned how to implement real-life phyiscs into code and it has intrigued my curiosity in the application of computer science in the real world. Additionally, this project helped me apply my skills in using the random import in python, object-oriented concepts and use advanced concepts such as inheritance. I hope to expand this project with the use of A.I.",
+            github: "https://github.com/omeradeel26/Asteroids-Game",
+            video: "https://www.youtube.com/embed/cawTgKl0w6M"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: 'processing',
+            year: "2021",
+            cont: "edu"
+        }
+    },
+    "Sudoku": {
+        name: "Sudoku",
+        image: "url(./media/sudoku.jpg)",
+        modal:{
+            name: "Sudoku",
+            date: "07/2020",
+            summary: "Using the GUI module PyGame in Python, I created the classic sit-down puzzle game sudoku. The game is played with a time limit, has a reset feature to play again and an auto-solve feature. The game was made using OOP and a game clock loop feature made seperate of pygame. ",
+            lesson: "In building this project, I was able to learn the GUI module PyGame which can be quite helpful in future projects. Additionally, I applied my knowledge of OOP to setup the gameboard and a backtracking recursion algorithim to create an auto-solve feature. In the future, I hope to add a solving animation and work more on my skills on creating an aesethetic UI.",
+            github: "https://github.com/omeradeel26/sudoku-solver",
+            video: "https://www.youtube.com/embed/wWq1MTI2VW4"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: "pygame",
+            year: "2021",
+            cont: "pers"
+        }
+    },
+
+    "Arduino Calculator": {
+        name: "Arduino Calculator",
+        image: "url(./media/calc.jpg)",
+        modal:{
+            name: "Arduino Calculator",
+            date: "06/2020",
+            summary: "Using an Arduino MicroController programmed in C language, for my grade 11 computer engineering final, I created a fully functioning calcualtor that can do addition, multiplication, division and subtraction operations on any amount of numbers.",
+            github: "https://github.com/omeradeel26/Arduino-Calculator",
+            lesson: "The project gave me a good understanding of how high-level microcontrollers such as Arduino work and how to use them to create projects. Additionally, interfacing with an LCD, buttons, LEDs, and a keypad with a live circuit gave me a better understanding of how hardware works. From here, I would like to use lower-level microcontrollers to get a barebones understanding of hardware." ,
+            video: "https://www.youtube.com/embed/KXoVBRioYZs"
+        }, 
+        filter: {
+            dev: "c", 
+            type: "arduino",
+            year: "2020",
+            cont: "edu"
+        }
+    },
+
+    "Hydro Grown": {
+        name: "Hydro Grown",
+        image: "url(./media/garden.jpg)",
+        modal:{
+            name: "Hydro Grown",
+            date: "05/2020",
+            summary: "Hydro Grown is a web-app made using Python Flask for the backend and HTML/CSS for the frontend. Based off the live-weather statistiscs of user's location it will tell users when to next water a plant species in their garden using an algorithim that factors in climate humidity, absorption rate of a species and precipitation levels.",
+            github: "https://github.com/omeradeel26/Hydro-Grown",
+            lesson: "As this project was made under a 24 hour time constraint in a hackathon, it pushed me to learn a new framework Python Flask. In learning Python Flask, I learned how to manipulate HTTP requests and attempted to connect it to a SQLite database. I also learned how to connect the frontend using templating in HTML/CSS.",
+            video: "https://www.youtube.com/embed/Ady_jsXAetg"
+        }, 
+        filter: {
+            dev: "html", 
+            type: "web",
+            year: "2020",
+            cont: "hack"
+        }
+    },
+
+    "Validdit": {
+        name: "Validdit",
+        image: "url(./media/reddit.jpeg)",
+        modal:{
+            name: "Validdit",
+            date: "05/26/2021",
+            summary: "this is an app",
+            github: "github/validdit",
+            lesson: "Don't use python",
+            video: "https://www.youtube.com/embed/LzDc8xGj_F4"
+        }, 
+        filter: {
+            dev: "html", 
+            type: "web",
+            year: "2020",
+            cont: "hack"
+        }
+    },
+
+    
+    "100 JS Challenge": {
+        name: "100 JS Challenge",
+        image: "url(./media/javascript.jpg)",
+        modal:{
+            name: "100 JS Challenge",
+            date: "05/26/2021",
+            summary: "this is an app",
+            github: "github/validdit",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "html", 
+            type: "static",
+            year: "2021",
+            cont: "pers"
+        }
+    },
+
+    "Web Portfolio 2020": {
+        name: "Web Portfolio 2020",
+        image: "url(./media/web-2020.png)",
+        modal:{
+            name: "Web Portfolio 2020",
+            date: "05/26/2021",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "html", 
+            type: "static",
+            year: "2020",
+            cont: "pers"
+        }
+    },
+
+    "Connect 4": {
+        name: "Connect 4",
+        image: "url(./media/connect.jpg)",
+        modal:{
+            name: "Connect 4",
+            date: "05/26/2021",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: "processing",
+            year: "2020",
+            cont: "edu"
+        }
+    },
+
+    "Binary Riddle": {
+        name: "Binary Riddle",
+        image: "url(./media/binary.jpg)",
+        modal:{
+            name: "Binary Riddle",
+            date: "11/2021",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "c", 
+            type: "arduino",
+            year: "2021",
+            cont: "edu"
+        }
+    },
+
+    "Accesso App": {
+        name: "Accesso App",
+        image: "url(./media/accesso.jpg)",
+        modal:{
+            name: "Accesso App",
+            date: "11/2021",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "html", 
+            type: "web",
+            year: "2021",
+            cont: "pers"
+        }
+    },
+
+
+    "Chess": {
+        name: "Chess",
+        image: "url(./media/chess.jpeg)",
+        modal:{
+            name: "Chess",
+            date: "01/2021",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: "processing",
+            year: "2021",
+            cont: "edu"
+        }
+    },
+
+    "Pong": {
+        name: "Pong",
+        image: "url(./media/pong.jpg)",
+        modal:{
+            name: "Pong",
+            date: "11/2020",
+            summary: "this is an app",
+            github: "github/website",
+            lesson: "Don't use python"
+        }, 
+        filter: {
+            dev: "pyth", 
+            type: "processing",
+            year: "2020",
+            cont: "edu"
+        }
+    }
+}
---
+++ projects.js
@@ -0,0 +1,236 @@
+//constants
+const backdrop = document.getElementById('overlay')
+const modal = document.getElementById('mod')
+const count = document.getElementById('counter')
+const project_grid = document.getElementById("projects_grid")
+
+//make global variables
+var pages = {
+  "1": []
+}
+var p = "1"
+
+//current page
+var c_page = "1"
+
+//log of all filters
+var filter_log = {
+    dev: "",
+    type: "",
+    year: "",
+    cont: ""
+}
+
+//load up all projects when loading window
+window.onload = all_projects()
+
+function all_projects(){
+  //reset variables  
+  pages = {
+    "1": []
+  }
+  p = "1"
+  c_page = "1"
+
+  var used = [] //used projects in pages
+
+  for (const [key, project] of Object.entries(data)){ //loop through each project
+    if ( (pages[p].length < 7) && (!used.includes(project.name))){
+      pages[p].push(project)
+      used.push(project.name)
+      if (pages[p].length == 6){
+        p = parseInt(p) +1
+        pages[p] = []
+      }
+    }
+  }
+
+  count.innerHTML = "Page " + c_page + " | " + p
+  disp_projects(pages["1"])
+}
+
+function disp_modal(info){
+  backdrop.style.display = "flex"
+
+
+
+  //retrieve info 
+
+  console.log(backdrop.style.display)
+
+  nam = info 
+  sum = data[info].modal.summary
+  less = data[info].modal.lesson
+  git = data[info].modal.github
+  dat = data[info].modal.date
+  vid = data[info].modal.video
+
+  document.getElementsByTagName('h4')[0].innerHTML = nam
+  document.getElementsByTagName('a')[0].href = git
+  document.getElementsByTagName('p')[0].innerHTML = dat
+  document.getElementsByTagName('p')[1].innerHTML = sum
+  document.getElementsByTagName('p')[2].innerHTML = less
+  document.getElementsByTagName("iframe")[0].src = vid
+}
+
+var stopAllYouTubeVideos = () => { 
+  var iframes = document.querySelectorAll('iframe');
+  Array.prototype.forEach.call(iframes, iframe => { 
+    iframe.src = iframe.src
+ });
+}
+
+document.addEventListener("click", function(e){
+  if (backdrop.style.display == "flex"){
+    if ((e.target.id == "close") || (e.target.id == "overlay")){
+      backdrop.style.display = "none"
+      stopAllYouTubeVideos()
+    }
+  }
+})
+
+function update_filter(id, checked){
+  //reset
+  pages = {
+    "1": []
+  }
+  c_page = "1"
+  p = "1"
+  project_grid.innerHTML = ''
+
+  // split into filter and type 
+  id = id.split('-')
+  filt = id[0]
+  type = id[1]
+
+  if (checked){
+    //remove other type in same category
+    if (filter_log[filt] != ""){
+      document.getElementById(filt+"-"+filter_log[filt]).checked = false
+    }
+    // add filter item
+    filter_log[filt] = type
+  } else {
+    //remove filter item
+    filter_log[filt] = ""
+  }
+
+  //if there are no filters set show all projects
+  if ((filter_log.dev == "") && (filter_log.type == "")  && (filter_log.year == "") && (filter_log.cont == "")) {
+    all_projects()
+  } else {
+  //get new projects with updated filter log
+    get_projects()
+  }
+}
+
+function get_projects(){
+  project_grid.innerHTML = ''
+
+  //track projects added to page so they are not repeated 
+  var used = [] //used projects in pages
+  var unused = [] //projects that cannot be used 
+
+  for (const [key, project] of Object.entries(data)){ //loop through each project
+    for (type of Object.values(filter_log)) { //loop through the filter log to compare to the projects
+      //get list of all filters on project
+      if (type != ""){
+        if ( (!Object.values(project.filter).includes(type)) && (!unused.includes(project.name)) ){
+          unused.push(project.name)
+        } 
+      }
+    }
+
+    // add te project to the page 
+    if ( (pages[p].length < 7) && (!used.includes(project.name)) && ((!unused.includes(project.name))) ){
+      pages[p].push(project)
+      used.push(project.name)
+      if (pages[p].length == 6){
+        p = parseInt(p) +1
+        pages[p] = []
+      }
+    }
+  }
+
+  //solve a bug
+  for ( const [key, val] of Object.entries(pages)){
+    if (val.length == 0){
+      delete(pages[key])
+      p -=1
+    }
+  }
+
+  count.innerHTML = "Page " + c_page + " | " + p
+  disp_projects(pages["1"])
+}
+
+
+function disp_projects(page){
+  project_grid.innerHTML = ''
+
+  var timer = 0;
+
+  for (project of page){
+    //create project div
+    const proj = document.createElement('div')
+    proj.classList.add("project_display")
+    //proj.classList.add("fade")
+    proj.setAttribute("id", project.name)
+
+    //set background image
+    proj.style.backgroundImage = project.image
+
+    //set title
+    var node = document.createElement("h4");
+    node.className = "modal-title"
+    node.innerHTML = project.name;
+    node.style.fontSize = "20px"
+    proj.appendChild(node);
+
+    //set link
+    var link = document.createElement("span")
+    link.className = "	fas fa-link modal-link"
+    link.style.fontSize = "20px"
+
+    //add button to link
+    link.addEventListener("click", function(e){
+      disp_modal(proj.id)    
+    }, false)
+
+    proj.appendChild(link)
+
+    //add black overlay
+    var overlay = document.createElement('div')
+    overlay.className = "project_overlay"
+    proj.appendChild(overlay)
+
+
+    //add fade effect
+    proj.style.transitionDuration = timer;
+
+    //add to parent grid
+    project_grid.appendChild(proj)
+  }
+}
+
+//go forward a page
+function forward_p(){
+  //if not exceeding limit
+  if (!(parseInt(c_page)+ 1 > p)){
+    //go forward a page
+    c_page = parseInt(c_page) + 1
+    disp_projects(pages[c_page])
+    count.innerHTML = "Page " + c_page + " | " + p
+  } 
+}
+
+//go backward a page
+function backward_p(){
+  //not exceeding limit
+  if (!(parseInt(c_page)- 1 < 1)){
+    // go back a page
+    c_page = parseInt(c_page) - 1
+    disp_projects(pages[c_page])
+    count.innerHTML = "Page " + c_page + " | " + p
+  } 
+}
